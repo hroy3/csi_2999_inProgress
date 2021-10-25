d84dee15e95132976c2d8e2fc09d6b586ec5271d
hroy3 <91563546+hroy3@users.noreply.github.com>
2021-10-25 14:40:48 -0400

    Updated css styling and html form fields to match database

--- Login_Signup_Pages/main.js
+++ Login_Signup_Pages/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const login_form = document.querySelector("#login");
     const signup_form = document.querySelector("#signup");
 
+
     document.querySelector("#signup_link").addEventListener("click", e => {
         e.preventDefault();
         login_form.classList.add("form_hidden");
@@ -14,5 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         signup_form.classList.add("form_hidden");
     });
 
+
     
 });
+
